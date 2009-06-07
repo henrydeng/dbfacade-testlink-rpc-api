29b3db9c570976076032a6e7b7c61720e0497cbc
danthehorn <danthehorn@yahoo.com>
2009-06-07 20:32:32 +0000

    Cleaned up bad code introduced when try to test eclipse plugin

--- dbfacade-testlink-rpc-api/src/org/dbfacade/testlink/tc/autoexec/EmptyExecutor.java
+++ dbfacade-testlink-rpc-api/src/org/dbfacade/testlink/tc/autoexec/EmptyExecutor.java
@@ -21,28 +21,43 @@
 package org.dbfacade.testlink.tc.autoexec;
 
 
-import java.util.Random;
-
 import org.dbfacade.testlink.api.client.TestLinkAPIException;
 
-
+/**
+ * This class can be used to write custom excutors by
+ * overriding the execute method.
+ * 
+ * It can also be used for testing since it will return
+ * an execution of failed if the default constructor is
+ * used or the result provided to the defaultResult
+ * constructor.
+ * 
+ * 
+ * @author Daniel Padilla
+ *
+ */
 public class EmptyExecutor implements TestCaseExecutor
 {
-	public static final short MODE_BOMB = 1;
-	public static final short MODE_TEST = 2;
-	private short mode = MODE_BOMB;
 	private short testState = STATE_READY;
-	private short testResult = RESULT_FAILED; 
+	private short testResult = RESULT_UNKNOWN; 
+	private short defaultResult = RESULT_FAILED;
 	
-	public EmptyExecutor()
-	{
-		mode = MODE_BOMB;
+	/**
+	 * Constructor in which the execution of the test
+	 * always returns failed.
+	 * 
+	 */
+	public EmptyExecutor() {
+		this(RESULT_FAILED);
 	}
 	
-	public EmptyExecutor(
-		short mode)
-	{
-		this.mode = mode;
+	/**
+	 * Constructor in which the execution of the test
+	 * always returns failed.
+	 * 
+	 */
+	public EmptyExecutor(short defaultResult) {
+		this.defaultResult = defaultResult;
 	}
 	
 	/**
@@ -106,33 +121,9 @@ public class EmptyExecutor implements TestCaseExecutor
 	public void execute(
 		TestCase testCase) throws TestLinkAPIException
 	{
-		if ( mode == MODE_BOMB ) {
-			setExecutionState(STATE_BOMBED);
-			throw new TestLinkAPIException(
-				"This is an empty executor to hold failed test results.");
-		} else {
-			Random rand = new Random();
-			int v = rand.nextInt(10);
-			if ( v < 2 ) {
-				setExecutionState(STATE_READY);
-			} else if ( v >= 2 && v < 4 ) {
-				setExecutionState(STATE_RUNNING);
-			} else if ( v >= 4 && v < 7 ) {
-				setExecutionState(STATE_COMPLETED);
-				if ( v == 4 ) {
-					setExecutionResult(RESULT_FAILED);
-				} else if ( v == 5 ) {
-					setExecutionResult(RESULT_PASSED);
-				} else {
-					setExecutionResult(RESULT_BLOCKED);
-				}
-			} else if ( v >= 7 && v < 9 ) {
-				setExecutionState(STATE_RESET);
-			} else {
-				setExecutionState(STATE_BOMBED);
-			}
-			
-		}
+		setExecutionState(STATE_RUNNING);
+		setExecutionResult(defaultResult);
+		setExecutionState(STATE_COMPLETED);
 	}
 
 }
