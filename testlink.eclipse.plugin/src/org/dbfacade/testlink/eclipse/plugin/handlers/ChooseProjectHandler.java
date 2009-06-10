ec4d470c9e59105c01704279c359f12d07fe080d
danthehorn <danthehorn@yahoo.com>
2009-06-10 02:01:09 +0000

    Made changes and now can configure a new launch. Still very much a work in progress.

---
+++ testlink.eclipse.plugin/src/org/dbfacade/testlink/eclipse/plugin/handlers/ChooseProjectHandler.java
@@ -0,0 +1,124 @@
+/*
+ * Daniel R Padilla
+ *
+ * Copyright (c) 2009, Daniel R Padilla
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.dbfacade.testlink.eclipse.plugin.handlers;
+
+
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.jdt.core.IJavaModel;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.ui.JavaElementLabelProvider;
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.dialogs.ElementListSelectionDialog;
+
+
+public class ChooseProjectHandler extends SelectorHandler
+{	
+	public ChooseProjectHandler(
+		Shell shell)
+	{
+		super(shell);
+	}
+
+	public void handle(
+		Label wLabel,
+		Text text,
+		Button button)
+	{
+		super.handle(wLabel, text, button);
+		IJavaProject project = chooseJavaProject();
+		if ( project == null ) {
+			return;
+		}
+    
+		String projectName = project.getElementName();
+		text.setText(projectName);
+	}
+	
+	/*
+	 * Realize a Java Project selection dialog and return the first selected project,
+	 * or null if there was none.
+	 */
+	private IJavaProject chooseJavaProject()
+	{
+		IJavaProject[] projects;
+		try {
+			projects = JavaCore.create(getWorkspaceRoot()).getJavaProjects();
+		} catch ( JavaModelException e ) {
+			// JUnitPlugin.log(e.getStatus());
+			projects = new IJavaProject[0];
+		}
+        
+		ILabelProvider labelProvider = new JavaElementLabelProvider(
+			JavaElementLabelProvider.SHOW_DEFAULT);
+		ElementListSelectionDialog dialog = new ElementListSelectionDialog(getShell(),
+			labelProvider);
+		dialog.setTitle("Blah (choose project");
+		dialog.setMessage("Bleh (choose project)");
+		dialog.setElements(projects);
+        
+		IJavaProject javaProject = getJavaProject();
+		if ( javaProject != null ) {
+			dialog.setInitialSelections(new Object[] { javaProject });
+		}
+		if ( dialog.open() == Window.OK ) {
+			return (IJavaProject) dialog.getFirstResult();
+		}
+		return null;
+	}
+    
+	/*
+	 * Convenience method to get the workspace root.
+	 */
+	private IWorkspaceRoot getWorkspaceRoot()
+	{
+		return ResourcesPlugin.getWorkspace().getRoot();
+	}
+    
+	/*
+	 * Return the IJavaProject corresponding to the project name in the project name
+	 * text field, or null if the text does not match a project name.
+	 */
+	private IJavaProject getJavaProject()
+	{
+		String projectName = text.getText().trim();
+		if ( projectName.length() < 1 ) {
+			return null;
+		}
+		return getJavaModel().getJavaProject(projectName);
+	}
+    
+	/*
+	 * Convenience method to get access to the java model.
+	 */
+	private IJavaModel getJavaModel()
+	{
+		return JavaCore.create(getWorkspaceRoot());
+	}
+    
+}
---
+++ testlink.eclipse.plugin/src/org/dbfacade/testlink/eclipse/plugin/handlers/SearchClassHandler.java
@@ -0,0 +1,124 @@
+/*
+ * Daniel R Padilla
+ *
+ * Copyright (c) 2009, Daniel R Padilla
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.dbfacade.testlink.eclipse.plugin.handlers;
+
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.jdt.core.IJavaModel;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+public class SearchClassHandler extends SelectorHandler {
+
+	public SearchClassHandler(Shell shell) {
+		super(shell);
+	}
+
+    /*
+     * Show a dialog that lists all main types
+     */
+	public void handle(
+			Label wLabel,
+			Text text,
+			Button button)
+		{
+			super.handle(wLabel, text, button);
+    	@SuppressWarnings("unused")
+		Shell shell = getShell();
+     
+        @SuppressWarnings("unused")
+		IJavaProject javaProject = getJavaProject();
+        
+        /*
+        IType[] types= new IType[0];
+        boolean[] radioSetting= new boolean[2];
+        try {
+            // fix for 66922 Wrong radio behaviour when switching
+			// remember the selected radio button
+			radioSetting[0]= fTestRadioButton.getSelection();
+            radioSetting[1]= fTestContainerRadioButton.getSelection();
+            
+            types= TestSearchEngine.findTests(getLaunchConfigurationDialog(), javaProject, getSelectedTestKind());
+        } catch (InterruptedException e) {
+            setErrorMessage(e.getMessage());
+            return;
+        } catch (InvocationTargetException e) {
+            JUnitPlugin.log(e.getTargetException());
+            return;
+        } finally {
+            fTestRadioButton.setSelection(radioSetting[0]);
+            fTestContainerRadioButton.setSelection(radioSetting[1]);
+        }
+
+        SelectionDialog dialog = new TestSelectionDialog(shell, types);
+        dialog.setTitle(JUnitMessages.JUnitLaunchConfigurationTab_testdialog_title);
+        dialog.setMessage(JUnitMessages.JUnitLaunchConfigurationTab_testdialog_message);
+        if (dialog.open() == Window.CANCEL) {
+            return;
+        }
+        
+        Object[] results = dialog.getResult();
+        if ((results == null) || (results.length < 1)) {
+            return;
+        }
+        IType type = (IType)results[0];
+        
+        if (type != null) {
+            fTestText.setText(type.getFullyQualifiedName('.'));
+            javaProject = type.getJavaProject();
+            fProjText.setText(javaProject.getElementName());
+        }
+        */
+    }
+       
+	/*
+	 * Convenience method to get the workspace root.
+	 */
+	private IWorkspaceRoot getWorkspaceRoot()
+	{
+		return ResourcesPlugin.getWorkspace().getRoot();
+	}
+    
+	/*
+	 * Return the IJavaProject corresponding to the project name in the project name
+	 * text field, or null if the text does not match a project name.
+	 */
+	private IJavaProject getJavaProject()
+	{
+		String projectName = text.getText().trim();
+		if ( projectName.length() < 1 ) {
+			return null;
+		}
+		return getJavaModel().getJavaProject(projectName);
+	}
+    
+	/*
+	 * Convenience method to get access to the java model.
+	 */
+	private IJavaModel getJavaModel()
+	{
+		return JavaCore.create(getWorkspaceRoot());
+	}
+}
