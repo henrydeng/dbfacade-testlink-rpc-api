0726b16d354cc293c95a5d2d00cbc3ce06e15114
danthehorn <danthehorn@yahoo.com>
2009-06-11 02:58:38 +0000

    We now get to the runner but we have to path

--- testlink.eclipse.plugin/src/org/dbfacade/testlink/eclipse/plugin/launcher/TestLinkLaunchConfigurationDelegate.java
+++ testlink.eclipse.plugin/src/org/dbfacade/testlink/eclipse/plugin/launcher/TestLinkLaunchConfigurationDelegate.java
@@ -19,30 +19,20 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+import org.dbfacade.testlink.eclipse.plugin.preferences.PreferenceConstants;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.core.runtime.SubProgressMonitor;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
-import org.eclipse.debug.core.ILaunchManager;
-import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.IMember;
-import org.eclipse.jdt.core.IMethod;
-import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.launching.AbstractJavaLaunchConfigurationDelegate;
 import org.eclipse.jdt.launching.ExecutionArguments;
+import org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants;
 import org.eclipse.jdt.launching.IVMRunner;
 import org.eclipse.jdt.launching.VMRunnerConfiguration;
 
 public class TestLinkLaunchConfigurationDelegate extends AbstractJavaLaunchConfigurationDelegate
 {
-	private boolean fKeepAlive= false;
-	private int fPort;
-	private IMember[] fTestElements;
-
-
 	@Override
 	public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor) throws CoreException {
 
@@ -60,43 +50,10 @@ public class TestLinkLaunchConfigurationDelegate extends AbstractJavaLaunchConfi
 
 		try {
 			
-			/*
-			if (mode.equals(JUnitLaunchConfigurationConstants.MODE_RUN_QUIETLY_MODE)) {
-				launch.setAttribute(JUnitLaunchConfigurationConstants.ATTR_NO_DISPLAY, "true"); //$NON-NLS-1$
-				mode = ILaunchManager.RUN_MODE;
-			}
-			*/
-
-			//monitor.subTask(JUnitMessages.JUnitLaunchConfigurationDelegate_verifying_attriburtes_description);
-
-			try {
-				preLaunchCheck(configuration, launch, new SubProgressMonitor(monitor, 2));
-			} catch (CoreException e) {
-				if (e.getStatus().getSeverity() == IStatus.CANCEL) {
-					monitor.setCanceled(true);
-					return;
-				}
-				throw e;
-			}
 			// check for cancellation
 			if (monitor.isCanceled()) {
 				return;
 			}
-
-			/*
-			fKeepAlive= mode.equals(ILaunchManager.DEBUG_MODE) && configuration.getAttribute(JUnitLaunchConfigurationConstants.ATTR_KEEPRUNNING, false);
-			*/
-			
-			fKeepAlive= mode.equals(ILaunchManager.DEBUG_MODE);
-
-			
-			// fPort= evaluatePort();
-			
-			/*
-			launch.setAttribute(JUnitLaunchConfigurationConstants.ATTR_PORT, String.valueOf(fPort));
-			*/
-
-			// fTestElements= evaluateTests(configuration, new SubProgressMonitor(monitor, 1));
 
 			String mainTypeName= verifyMainTypeName(configuration);
 			IVMRunner runner= getVMRunner(configuration, mode);
@@ -163,47 +120,11 @@ public class TestLinkLaunchConfigurationDelegate extends AbstractJavaLaunchConfi
 	}
 	
 
-	/**
-	 * Performs a check on the launch configuration's attributes. If an attribute contains an invalid value, a {@link CoreException}
-	 * with the error is thrown.
-	 *
-	 * @param configuration the launch configuration to verify
-	 * @param launch the launch to verify
-	 * @param monitor the progress monitor to use
-	 * @throws CoreException an exception is thrown when the verification fails
-	 */
-	protected void preLaunchCheck(ILaunchConfiguration configuration, ILaunch launch, IProgressMonitor monitor) throws CoreException {
-		try {
-			@SuppressWarnings("unused")
-			IJavaProject javaProject= getJavaProject(configuration);
-			
-			/*
-			if ((javaProject == null) || !javaProject.exists()) {
-				informAndAbort(JUnitMessages.JUnitLaunchConfigurationDelegate_error_invalidproject, null, IJavaLaunchConfigurationConstants.ERR_NOT_A_JAVA_PROJECT);
-			}
-			if (!TestSearchEngine.hasTestCaseType(javaProject)) {
-				informAndAbort(JUnitMessages.JUnitLaunchConfigurationDelegate_error_junitnotonpath, null, IJUnitStatusConstants.ERR_JUNIT_NOT_ON_PATH);
-			}
-			*/
-			
-			/*
-			ITestKind testKind= getTestRunnerKind(configuration);
-			boolean isJUnit4Configuration= TestKindRegistry.JUNIT4_TEST_KIND_ID.equals(testKind.getId());
-			if (isJUnit4Configuration && ! TestSearchEngine.hasTestAnnotation(javaProject)) {
-				informAndAbort(JUnitMessages.JUnitLaunchConfigurationDelegate_error_junit4notonpath, null, IJUnitStatusConstants.ERR_JUNIT_NOT_ON_PATH);
-			}
-			*/
-		} finally {
-			monitor.done();
-		}
-	}
-
 	/* (non-Javadoc)
 	 * @see org.eclipse.jdt.launching.AbstractJavaLaunchConfigurationDelegate#verifyMainTypeName(org.eclipse.debug.core.ILaunchConfiguration)
 	 */
 	public String verifyMainTypeName(ILaunchConfiguration configuration) throws CoreException {
-		//return "org.eclipse.jdt.internal.junit.runner.RemoteTestRunner"; //$NON-NLS-1$
-		return "org.dbfacade.testlink.eclipse.plugin.Runner";
+		return "org.dbfacade.testlink.eclipse.plugin.launcher.TestLinkRunner";
 	}
 
 
@@ -215,57 +136,54 @@ public class TestLinkLaunchConfigurationDelegate extends AbstractJavaLaunchConfi
 	 * @param programArguments a {@link List} of {@link String} representing the resulting program arguments
 	 * @exception CoreException if unable to collect the execution arguments
 	 */
-	protected void collectExecutionArguments(ILaunchConfiguration configuration, List<String> vmArguments, List<String> programArguments) throws CoreException {
+	protected void collectExecutionArguments(ILaunchConfiguration config, List<String> vmArguments, List<String> programArguments) throws CoreException {
 
-		// add program & VM arguments provided by getProgramArguments and getVMArguments
-		String pgmArgs= getProgramArguments(configuration);
-		String vmArgs= getVMArguments(configuration);
+		// Standard code to initialize paramters
+		String pgmArgs= getProgramArguments(config);
+		String vmArgs= getVMArguments(config);
 		ExecutionArguments execArgs= new ExecutionArguments(vmArgs, pgmArgs);
 		vmArguments.addAll(Arrays.asList(execArgs.getVMArgumentsArray()));
 		programArguments.addAll(Arrays.asList(execArgs.getProgramArgumentsArray()));
+		
+		// Initialize from a saved copy of the configuration
+		setParamFromConfig(IJavaLaunchConfigurationConstants.ATTR_PROJECT_NAME,
+			 config, programArguments);
+		setParamFromConfig(PreferenceConstants.P_DEFAULT_PROJECT_NAME,
+			 config, programArguments);
+		setParamFromConfig(PreferenceConstants.P_DEV_KEY, 
+			config, programArguments);
+		setParamFromConfig(PreferenceConstants.P_TESTLINK_URL,
+			 config, programArguments);
+		setParamFromConfig(PreferenceConstants.P_DEFAULT_TESTPLAN_PREP_CLASS,
+			config, programArguments);
+		setParamFromConfig(PreferenceConstants.P_OPTIONAL_EXTERNAL_CONFIG_FILE,
+			 config, programArguments);
 
-		// String testFailureNames= configuration.getAttribute(JUnitLaunchConfigurationConstants.ATTR_FAILURES_NAMES, ""); //$NON-NLS-1$
-
-		programArguments.add("-version"); //$NON-NLS-1$
-		programArguments.add("3"); //$NON-NLS-1$
-
-		programArguments.add("-port"); //$NON-NLS-1$
-		programArguments.add(String.valueOf(fPort));
-
-		if (fKeepAlive)
-			programArguments.add(0, "-keepalive"); //$NON-NLS-1$
-
-		//ITestKind testRunnerKind= getTestRunnerKind(configuration);
-
-		programArguments.add("-testLoaderClass"); //$NON-NLS-1$
-		// programArguments.add(testRunnerKind.getLoaderClassName());
-		programArguments.add("-loaderpluginname"); //$NON-NLS-1$
-		// programArguments.add(testRunnerKind.getLoaderPluginId());
-
-		IMember[] testElements = fTestElements;
-
-		// a test name was specified just run the single test
-		if (testElements.length == 1) {
-			if (testElements[0] instanceof IMethod) {
-				IMethod method= (IMethod) testElements[0];
-				programArguments.add("-test"); //$NON-NLS-1$
-				programArguments.add(method.getDeclaringType().getFullyQualifiedName()+':'+method.getElementName());
-			} else if (testElements[0] instanceof IType) {
-				IType type= (IType) testElements[0];
-				programArguments.add("-classNames"); //$NON-NLS-1$
-				programArguments.add(type.getFullyQualifiedName());
-			} else {
-				// informAndAbort(JUnitMessages.JUnitLaunchConfigurationDelegate_error_wrong_input, null, IJavaLaunchConfigurationConstants.ERR_UNSPECIFIED_MAIN_TYPE);
-			}
-		} else if (testElements.length > 1) {
-			// String fileName= createTestNamesFile(testElements);
-			// programArguments.add("-testNameFile"); //$NON-NLS-1$
-			// programArguments.add(fileName);
+		
+	}
+	
+	/*
+	 * Initialize the variables from this class using what comes back from
+	 * the configuration that is returned by the caller to the configuration
+	 * objects that exist.
+	 */
+	private void setParamFromConfig(
+		String key,
+		ILaunchConfiguration config,
+		List<String> programArguments)
+	{
+		if ( key == null || config == null ) {
+			return;
 		}
-		//if (testFailureNames.length() > 0) {
-		//	programArguments.add("-testfailures"); //$NON-NLS-1$
-		//	programArguments.add(testFailureNames);
-		//}
+		String value = ""; 
+		try {
+			value = config.getAttribute(key, ""); 
+		} catch ( CoreException ce ) {}
+		
+		if ( value != null ) {
+			programArguments.add(key);
+			programArguments.add(value.toString()); 
+		}
 	}
 
 
