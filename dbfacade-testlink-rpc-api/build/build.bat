e950f6060c7c4fa95abe6ab8bdce08416075d1d7
brunodepaulak <brunodepaulak@gmail.com>
2010-08-28 03:31:40 +0000

    Project dbfacade-testlink-rpc-api written using maven-2.2.1.
    - build directory removed
    - lib directory and its jars removed
    - src directory moved to src/main/java
    - junit package and its classes moved to src/main/test
    - added pom.xml
    - basic project info added to pom.xml
    - dependencies added to pom.xml
    
    Once you download it, run mvn -U -e eclipse:eclipse and it will set up the eclipse project. 
    
    Before you synchronize it, run mvn -U -e clean eclipse:clean.

--- dbfacade-testlink-rpc-api/build/build.bat
+++
@@ -1,3 +0,0 @@
-call ant
-
-pause
--- testlink.eclipse.plugin/src/testlink/eclipse/plugin/views/HtmlMessageText.java
+++
@@ -1,9 +0,0 @@
-package testlink.eclipse.plugin.views;
-
-public interface HtmlMessageText {
-	public static final String OPEN_HTML_DOC="<html><body><p>";
-	public static final String CLOSE_HTML_DOC="</p></body></html>";
-
-	public void setHtmlText(String text);
-	public String getHtmlText();
-}
